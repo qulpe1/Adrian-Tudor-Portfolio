27e31a432e751862e30ca0c30e4a1dc1229b65b8
qulpe1 <58472647+qulpe1@users.noreply.github.com>
2020-05-08 14:33:50 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkI2ASM8SqUBtynZb3PzUD85pLpTd32a0",
+    authDomain: "first-deploy-52e7b.firebaseapp.com",
+    databaseURL: "https://first-deploy-52e7b.firebaseio.com",
+    projectId: "first-deploy-52e7b",
+    storageBucket: "first-deploy-52e7b.appspot.com",
+    messagingSenderId: "426142080445",
+    appId: "1:426142080445:web:46c241b5024884e93cc6d4",
+    measurementId: "G-JKJYLPSNLX"};
+
+
+
